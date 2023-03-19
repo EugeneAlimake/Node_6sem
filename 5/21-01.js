fd6a86f32e215145e2ad36723d022735ff9045a8
Eugene AliMake <eugenealimake@gmail.com>
2023-03-19 18:13:10 +0300

    new

--- 5/21-01.js
+++ 5/21-01.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'/static')));
 
-const hbs = require('express-handlebars').create({extname:'.hbs',  defaultLayout: false, helpers:{canbutton:()=>{ return `<input class="boton1"  value="Отказаться" onclick="window.location = '/'"></input>`}}});
-
+const hbs = require('express-handlebars').create({extname:'.hbs',  defaultLayout: false, layoutsDir:"views", helpers:{canbutton:()=>{ return `<input class="boton1"  value="Отказаться" onclick="window.location = '/'"></input>`}}});
+app.use(express.static(path.join(__dirname + '/views')));
 const urlencodedParser = express.urlencoded({extended: false});
 app.use(urlencodedParser);
 app.use(methodOverride('_method'));
