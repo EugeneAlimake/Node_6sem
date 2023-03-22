6b8ef179b955b755fa43322101c3bfc12a9264e5
Eugene AliMake <eugenealimake@gmail.com>
2023-03-22 10:33:11 +0300

    new

--- 5/21-01.js
+++ 5/21-01.js
@@ -3,12 +3,13 @@ const methodOverride = require('method-override');
 const bodyParser = require('body-parser')
 const PhoneRoute = require('./routes/PhoneRoute');
 const path = require("path");
+const cors = require('cors');
 const app = express();
 
 
 const urlencodedParser = express.urlencoded({extended: false});
 const hbs = require('express-handlebars').create({extname:'.hbs',  defaultLayout: false, layoutsDir:"/views", helpers:{canbutton:()=>{ return `<input class="boton1"  value="Отказаться" onclick="window.location = '/'"></input>`}}});
-const cors = require('cors');
+
 
 app.engine('.hbs', hbs.engine);
 app.set("view engine", ".hbs");
@@ -16,7 +17,8 @@ app.set('port',3000);
 
 app.set('/views', express.static(path.join(__dirname,'/views')));
 const corsOptions = {
-    origin: 'https://nodejs-3etv.onrender.com'
+    origin: 'https://nodejs-3etv.onrender.com',
+    method:"GET,HEAD,PUT,POST, PATCH,DELETE"
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
